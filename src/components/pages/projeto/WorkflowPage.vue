8b8a0f358d00ae9ca9a3a25fe512987907c90ccd
Eduardo Purificacao <eduardo.purificacao@claro.com.br>
2026-08-18 15:16:17 -0300

    Adicionado icon para visao os processo no Workflow

--- src/components/pages/projeto/WorkflowPage.vue
+++ src/components/pages/projeto/WorkflowPage.vue
@@ -5,14 +5,83 @@
     :fields="fields"
     :headers="headers"
     data-testid="workflow-crud"
-  />
+  >
+    <template #actionsField="{ item }">
+      <ButtonComponent
+        icon="mdi-transit-connection-variant"
+        variant="text"
+        size="small"
+        color="primary"
+        data-testid="workflow-btn-mobilidades"
+        @click="openModalMobilidades(item)"
+      />
+    </template>
+  </CrudComponent>
+
+  <v-dialog
+    v-if="dialog"
+    v-model="dialog"
+    max-width="1400px"
+    scrollable
+    @keydown.esc="closeModalMobilidades"
+  >
+    <CardComponent rounded="lg">
+      <CardTitleComponent class="d-flex align-center ga-2 py-4 px-6 border-b">
+        <IconComponent
+          color="primary"
+          size="22"
+        >
+          mdi-transit-connection-variant
+        </IconComponent>
+        <span class="text-h6">Mobilidades do workflow: {{ workflowSelecionado?.workflow }}</span>
+        <v-spacer />
+        <ButtonComponent
+          icon="mdi-close"
+          variant="text"
+          size="small"
+          data-testid="workflow-modal-btn-fechar"
+          @click="closeModalMobilidades"
+        />
+      </CardTitleComponent>
+
+      <CardTextComponent class="pt-4">
+        <FlowMobilidadeComponent
+          v-if="workflowSelecionado"
+          :ctrl-workflow-id="workflowSelecionado.id"
+          height="700px"
+        />
+      </CardTextComponent>
+    </CardComponent>
+  </v-dialog>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import CrudComponent from '@/components/crud/CrudComponent.vue'
+import FlowMobilidadeComponent from '@/components/pages/projeto/tarefa/FlowMobilidadeComponent.vue'
+import CardComponent from '@/components/comuns/cards/CardComponent.vue'
+import CardTitleComponent from '@/components/comuns/cards/CardTitleComponent.vue'
+import CardTextComponent from '@/components/comuns/cards/CardTextComponent.vue'
+import ButtonComponent from '@/components/comuns/buttons/ButtonComponent.vue'
+import IconComponent from '@/components/comuns/icons/IconComponent.vue'
 import {useCrud} from "@/services/useCrud.js";
 const { index: fetchOrganizacoes } = useCrud('app/organizacoes')
 const { index: fetchTarefa } = useCrud('wf/tarefas')
+
+const dialog = ref(false)
+const workflowSelecionado = ref(null)
+
+const openModalMobilidades = (item) => {
+  workflowSelecionado.value = item
+  dialog.value = true
+}
+
+const closeModalMobilidades = () => {
+  dialog.value = false
+  setTimeout(() => {
+    workflowSelecionado.value = null
+  }, 300)
+}
 
 const fields = [
   {
